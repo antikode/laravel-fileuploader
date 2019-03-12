8f47d5ff1796a17e512787209b364de45ede6a05
Rio <alfarioekaputra@gmail.com>
2019-03-12 11:39:14 +0700

    change theme to thumbnail

--- src/resources/assets/js/fileuploader.js
+++ src/resources/assets/js/fileuploader.js
@@ -4,13 +4,65 @@ $(document).ready(function() {
 	// enable fileuploader plugin
 	$('#fileuploader-voyager').fileuploader({
         extensions: ['jpg', 'jpeg', 'png', 'gif', 'bmp'],
-        addMore: true,
-        thumbnails: {
-            onItemShow: function(item) {
-                // add sorter button to the item html
-                item.html.find('.fileuploader-action-remove').before('<a class="fileuploader-action fileuploader-action-sort" title="Sort"><i></i></a>');
-            }
-        },
+        changeInput: ' ',
+        theme: 'thumbnails',
+        enableApi: true,
+		addMore: true,
+		thumbnails: {
+			box: '<div class="fileuploader-items">' +
+                      '<ul class="fileuploader-items-list">' +
+					      '<li class="fileuploader-thumbnails-input"><div class="fileuploader-thumbnails-input-inner"><i>+</i></div></li>' +
+                      '</ul>' +
+                  '</div>',
+			item: '<li class="fileuploader-item file-has-popup">' +
+				       '<div class="fileuploader-item-inner">' +
+                           '<div class="type-holder">${extension}</div>' +
+                           '<div class="actions-holder">' +
+						   	   '<a class="fileuploader-action fileuploader-action-remove" title="${captions.remove}"><i></i></a>' +
+                           '</div>' +
+                           '<div class="thumbnail-holder">' +
+                               '${image}' +
+                               '<span class="fileuploader-action-popup"></span>' +
+                           '</div>' +
+                           '<div class="content-holder"><h5>${name}</h5><span>${size2}</span></div>' +
+                       	   '<div class="progress-holder">${progressBar}</div>' +
+                       '</div>' +
+                  '</li>',
+			item2: '<li class="fileuploader-item file-has-popup">' +
+				       '<div class="fileuploader-item-inner">' +
+                           '<div class="type-holder">${extension}</div>' +
+                           '<div class="actions-holder">' +
+						   	   '<a href="${file}" class="fileuploader-action fileuploader-action-download" title="${captions.download}" download><i></i></a>' +
+						   	   '<a class="fileuploader-action fileuploader-action-remove" title="${captions.remove}"><i></i></a>' +
+                           '</div>' +
+                           '<div class="thumbnail-holder">' +
+                               '${image}' +
+                               '<span class="fileuploader-action-popup"></span>' +
+                           '</div>' +
+                           '<div class="content-holder"><h5>${name}</h5><span>${size2}</span></div>' +
+                       	   '<div class="progress-holder">${progressBar}</div>' +
+                       '</div>' +
+                   '</li>',
+			startImageRenderer: true,
+            canvasImage: false,
+			_selectors: {
+				list: '.fileuploader-items-list',
+				item: '.fileuploader-item',
+				start: '.fileuploader-action-start',
+				retry: '.fileuploader-action-retry',
+				remove: '.fileuploader-action-remove'
+			},
+			onItemShow: function(item, listEl, parentEl, newInputEl, inputEl) {
+				var plusInput = listEl.find('.fileuploader-thumbnails-input'),
+                    api = $.fileuploader.getInstance(inputEl.get(0));
+				
+                plusInput.insertAfter(item.html)[api.getOptions().limit && api.getChoosedFiles().length >= api.getOptions().limit ? 'hide' : 'show']();
+				
+				if(item.format == 'image') {
+					item.html.find('.fileuploader-item-icon').hide();
+				}
+			}
+		},
         dragDrop: {
             container: '.fileuploader-thumbnails-input'
         },
--- webpack.mix.js
+++ webpack.mix.js
@@ -5,9 +5,10 @@ mix.js([
     'src/resources/assets/js/fileuploader.js'
 ], 'src/public/assets/js/app.js')
 .styles([
-    'src/resources/assets/css/jquery.fileuploader.min.css',
-], 'src/public/assets/css/app.css')
-.styles([
     'src/resources/assets/css/font/font-fileuploader.css'
 ], 'src/public/assets/css/font/font.css')
+.styles([
+    'src/resources/assets/css/jquery.fileuploader.min.css',
+    'src/resources/assets/css/jquery.fileuploader-theme-thumbnails.css',
+], 'src/public/assets/css/app.css')
 .copyDirectory('src/resources/assets/css/font', 'src/public/assets/css/font');
