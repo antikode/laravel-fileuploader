92c57c40b52340a51a547b2811b45078ce62ddd6
Rio <alfarioekaputra@gmail.com>
2019-03-26 18:53:21 +0700

    multiple fileuploader

--- src/resources/assets/js/fileuploader.js
+++ src/resources/assets/js/fileuploader.js
@@ -1,103 +1,70 @@
-$(document).ready(function() {
-	$('#images_data').val();
-	
-	// enable fileuploader plugin
-	$('.fileuploader-voyager').fileuploader({
-        extensions: ['jpg', 'jpeg', 'png', 'gif', 'bmp'],
-        changeInput: ' ',
-        theme: 'thumbnails',
-        enableApi: true,
-		addMore: true,
-		thumbnails: {
-			box: '<div class="fileuploader-items">' +
-                      '<ul class="fileuploader-items-list">' +
-					      '<li class="fileuploader-thumbnails-input"><div class="fileuploader-thumbnails-input-inner"><i>+</i></div></li>' +
-                      '</ul>' +
-                  '</div>',
-			item: '<li class="fileuploader-item file-has-popup">' +
-				       '<div class="fileuploader-item-inner">' +
-                           '<div class="type-holder">${extension}</div>' +
-                           '<div class="actions-holder">' +
-						   	   '<a class="fileuploader-action fileuploader-action-remove" title="${captions.remove}"><i></i></a>' +
-                           '</div>' +
-                           '<div class="thumbnail-holder">' +
-                               '${image}' +
-                               '<span class="fileuploader-action-popup"></span>' +
-                           '</div>' +
-                           '<div class="content-holder"><h5>${name}</h5><span>${size2}</span></div>' +
-                       	   '<div class="progress-holder">${progressBar}</div>' +
-                       '</div>' +
-                  '</li>',
-			item2: '<li class="fileuploader-item file-has-popup">' +
-				       '<div class="fileuploader-item-inner">' +
-                           '<div class="type-holder">${extension}</div>' +
-                           '<div class="actions-holder">' +
-						   	   '<a href="${file}" class="fileuploader-action fileuploader-action-download" title="${captions.download}" download><i></i></a>' +
-						   	   '<a class="fileuploader-action fileuploader-action-remove" title="${captions.remove}"><i></i></a>' +
-                           '</div>' +
-                           '<div class="thumbnail-holder">' +
-                               '${image}' +
-                               '<span class="fileuploader-action-popup"></span>' +
-                           '</div>' +
-                           '<div class="content-holder"><h5>${name}</h5><span>${size2}</span></div>' +
-                       	   '<div class="progress-holder">${progressBar}</div>' +
-                       '</div>' +
-                   '</li>',
-			startImageRenderer: true,
-            canvasImage: false,
-			_selectors: {
-				list: '.fileuploader-items-list',
-				item: '.fileuploader-item',
-				start: '.fileuploader-action-start',
-				retry: '.fileuploader-action-retry',
-				remove: '.fileuploader-action-remove'
-			},
-			onItemShow: function(item, listEl, parentEl, newInputEl, inputEl) {
-				var plusInput = listEl.find('.fileuploader-thumbnails-input'),
-                    api = $.fileuploader.getInstance(inputEl.get(0));
-				
-                plusInput.insertAfter(item.html)[api.getOptions().limit && api.getChoosedFiles().length >= api.getOptions().limit ? 'hide' : 'show']();
-				
-				if(item.format == 'image') {
-					item.html.find('.fileuploader-item-icon').hide();
-				}
-			}
-		},
-        dragDrop: {
-            container: '.fileuploader-thumbnails-input'
-        },
-        afterRender: function(listEl, parentEl, newInputEl, inputEl) {
-            var plusInput = listEl.find('.fileuploader-thumbnails-input'),
-                api = $.fileuploader.getInstance(inputEl.get(0));
-        
-            plusInput.on('click', function() {
-                api.open();
-            });
-        },
-        
-		sorter: {
-			selectorExclude: null,
-			placeholder: null,
-			scrollContainer: window,
-			onSort: function(list, listEl, parentEl, newInputEl, inputEl) {
-                // onSort callback
-                $('#images_data_sort').val();
-                const sorts = [];
-                $.each(list, function(index, value){
-                    sorts.push({
-                        name: value.name,
-                        type : value.type,
-                        size : value.size,
-                        file : value.file,
-                        data : {
-                            url : value.data.url,
-                        }
-                    });
-                });
-                
-                $('#images_data_sort').val(JSON.stringify(sorts));
-			}
-		}
-	});
-    
-});
+$('.row_field').each(function(index, value){
+    $('#images_data_'+$(value).val()).val();
+      // enable fileuploader plugin
+      $('#fileuploader-voyager-'+$(value).val()).fileuploader({
+          extensions: ['jpg', 'jpeg', 'png', 'gif', 'bmp'],
+          changeInput: ' ',
+          theme: 'thumbnails',
+          enableApi: true,
+          addMore: true,
+          thumbnails: {
+              box: '<div class="fileuploader-items">' + '<ul class="fileuploader-items-list">' + '<li class="fileuploader-thumbnails-input"><div class="fileuploader-thumbnails-input-inner"><i>+</i></div></li>' + '</ul>' + '</div>',
+              item: '<li class="fileuploader-item file-has-popup">' + '<div class="fileuploader-item-inner">' + '<div class="type-holder">${extension}</div>' + '<div class="actions-holder">' + '<a class="fileuploader-action fileuploader-action-remove" title="${captions.remove}"><i></i></a>' + '</div>' + '<div class="thumbnail-holder">' + '${image}' + '<span class="fileuploader-action-popup"></span>' + '</div>' + '<div class="content-holder"><h5>${name}</h5><span>${size2}</span></div>' + '<div class="progress-holder">${progressBar}</div>' + '</div>' + '</li>',
+              item2: '<li class="fileuploader-item file-has-popup">' + '<div class="fileuploader-item-inner">' + '<div class="type-holder">${extension}</div>' + '<div class="actions-holder">' + '<a href="${file}" class="fileuploader-action fileuploader-action-download" title="${captions.download}" download><i></i></a>' + '<a class="fileuploader-action fileuploader-action-remove" title="${captions.remove}"><i></i></a>' + '</div>' + '<div class="thumbnail-holder">' + '${image}' + '<span class="fileuploader-action-popup"></span>' + '</div>' + '<div class="content-holder"><h5>${name}</h5><span>${size2}</span></div>' + '<div class="progress-holder">${progressBar}</div>' + '</div>' + '</li>',
+              startImageRenderer: true,
+              canvasImage: false,
+              _selectors: {
+                  list: '.fileuploader-items-list',
+                  item: '.fileuploader-item',
+                  start: '.fileuploader-action-start',
+                  retry: '.fileuploader-action-retry',
+                  remove: '.fileuploader-action-remove'
+              },
+              onItemShow: function onItemShow(item, listEl, parentEl, newInputEl, inputEl) {
+                  var plusInput = listEl.find('.fileuploader-thumbnails-input'),
+                      api = $.fileuploader.getInstance(inputEl.get(0));
+  
+                  plusInput.insertAfter(item.html)[api.getOptions().limit && api.getChoosedFiles().length >= api.getOptions().limit ? 'hide' : 'show']();
+  
+                  if (item.format == 'image') {
+                      item.html.find('.fileuploader-item-icon').hide();
+                  }
+              }
+          },
+          dragDrop: {
+              container: '.fileuploader-thumbnails-input'
+          },
+          afterRender: function afterRender(listEl, parentEl, newInputEl, inputEl) {
+              var plusInput = listEl.find('.fileuploader-thumbnails-input'),
+                  api = $.fileuploader.getInstance(inputEl.get(0));
+  
+              plusInput.on('click', function () {
+                  api.open();
+              });
+          },
+  
+          sorter: {
+              selectorExclude: null,
+              placeholder: null,
+              scrollContainer: window,
+              onSort: function onSort(list, listEl, parentEl, newInputEl, inputEl) {
+                  // onSort callback
+                  $('#images_data_sort_'+$(value).val()).val();
+                  var sorts = [];
+                  $.each(list, function (index, value) {
+                      sorts.push({
+                          name: value.name,
+                          type: value.type,
+                          size: value.size,
+                          file: value.file,
+                          data: {
+                              url: value.data.url
+                          }
+                      });
+                  });
+  
+                  $('#images_data_sort_'+$(value).val()).val(JSON.stringify(sorts));
+              }
+          }
+      });
+  });
--- src/views/formfields/fileuploader.blade.php
+++ src/views/formfields/fileuploader.blade.php
@@ -33,12 +33,14 @@
 <input type="file"
     class="form-control fileuploader-voyager"
     name={{ $row->field }}[]
-    id="fileuploader-voyager"
+    id="fileuploader-voyager-{{$row->field}}"
     data-name="{{ $row->display_name }}"
     @if($row->required == 1) required @endif
     step="any"
     placeholder="{{ isset($options->placeholder)? old($row->field, $options->placeholder): $row->display_name }}"
     data-fileuploader-files={{ isset($preloadedFiles) ? $preloadedFiles : '' }}>
 
-<input type="hidden" name="images_data" id="images_data" value="{{($dataTypeContent->{$row->field})}}">
-<input type="hidden" name="images_data_sort" id="images_data_sort" value="{{($dataTypeContent->{$row->field})}}">
+<input type="hidden" value="{{$row->field}}" class="row_field">
+<input type="hidden" name="images_data" id="images_data_{{$row->field}}" value="{{($dataTypeContent->{$row->field})}}">
+<input type="hidden" name="images_data_sort" id="images_data_sort_{{$row->field}}" value="{{($dataTypeContent->{$row->field})}}">
+<input type="hidden" name="file_uploader_id" value="fileuploader-voyager-{{$row->field}}" id="file_uploader_id" class="file_uploader_id">
